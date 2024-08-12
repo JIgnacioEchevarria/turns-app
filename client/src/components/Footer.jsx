c2117ff64a286c5691dd2fcccb9112f270517203
JIgnacioEchevarria <echevarriajignacio@gmail.com>
2024-08-12 11:46:29 -0300

    Modificación Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import MailOutlineIcon from '@mui/icons-material/MailOutline'
-import InstagramIcon from '@mui/icons-material/Instagram'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
 
 export const Footer = () => {
@@ -8,7 +8,7 @@ export const Footer = () => {
       <h4>JIE Turnos</h4>
       <ul className='contact-list'>
         <li><a href=""><MailOutlineIcon sx={{ fontSize: '2rem', color: '#FFFFFF', cursor: 'pointer' }} /></a></li>
-        <li><a href="https://www.instagram.com/nachoechevarria3/"><InstagramIcon sx={{ fontSize: '2rem', color: '#FFFFFF', cursor: 'pointer' }} /></a></li>
+        <li><a href="https://www.linkedin.com/in/juan-ignacio-echevarria-524356322"><LinkedInIcon sx={{ fontSize: '2rem', color: '#FFFFFF', cursor: 'pointer' }} /></a></li>
         <li><a href="https://github.com/JIgnacioEchevarria"><GitHubIcon sx={{ fontSize: '2rem', color: '#FFFFFF', cursor: 'pointer' }} /></a></li>
       </ul>
     </footer>
